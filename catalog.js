165ec9a1c3a699801878bef65df8815d790d85b1
NongJie <245722665@qq.com>
2024-09-30 06:33:30 +0000

    update catalog.js.
    Signed-off-by: NongJie <245722665@qq.com>

--- catalog.js
+++ catalog.js
@@ -3,3 +3,12 @@ if (!localStorage.getItem('old-extension-tip')) {
 		'，请谨慎下载');
 	localStorage.setItem('old-extension-tip', 'true');
 };
+extension["无名扩展"] = {
+	date:"2022/01/11",
+	intro: '',
+	author: "te",
+	netdisk: "",
+	forum: "",
+	version: "1.0",
+	size: "2.66MB"
+};
