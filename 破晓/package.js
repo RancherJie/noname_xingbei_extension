f31be70d5db930aa7f419437949baf3a7a33f3b3
jjl <10549727+llsang932093346@user.noreply.gitee.com>
2024-09-30 14:54:07 +0800

    加入package.js

---
+++ 破晓/package.js
@@ -0,0 +1,10 @@
+extension["破晓"]={
+    date: "2024/09/30",
+	intro:"复活了破晓中的几个好玩的角色，占星和大地武士缺少高清卡图，能提供的话感激不尽。",
+	author:"连桑",
+	netdisk:"",
+	forum:"",
+	version:"0.1",
+	files:["daDiWuShi.jpg","caiJueZheP.jpg","zhanXingJia.jpg","xinLinSuShi.jpg"],
+	size:"906KB"
+};
