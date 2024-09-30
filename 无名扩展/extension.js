21b14495a6fa26d65999f618ba142bfbda93198c
NongJie <245722665@qq.com>
2024-09-30 06:34:35 +0000

    1
    Signed-off-by: NongJie <245722665@qq.com>

---
+++ 无名扩展/extension.js
@@ -0,0 +1,30 @@
+game.import("extension",function(lib,game,ui,get,ai,_status){return {name:"无名扩展",content:function (config,pack){
+    
+},precontent:function (){
+    
+},config:{},help:{},package:{
+    character:{
+        character:{
+        },
+        translate:{
+        },
+    },
+    card:{
+        card:{
+        },
+        translate:{
+        },
+        list:[],
+    },
+    skill:{
+        skill:{
+        },
+        translate:{
+        },
+    },
+    intro:"",
+    author:"农之",
+    diskURL:"",
+    forumURL:"",
+    version:"1.0",
+},files:{"character":[],"card":[],"skill":[],"audio":[]}}})
