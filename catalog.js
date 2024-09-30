2ee8ad2629956286722be312a93c015208fba024
NongJie <245722665@qq.com>
2024-09-30 05:46:00 +0000

    update catalog.js.
    Signed-off-by: NongJie <245722665@qq.com>

--- catalog.js
+++ catalog.js
@@ -11,5 +11,5 @@ extension["破晓"] = {
     netdisk: "",
     forum: "",
     version:"0.1",
-    files:{"character":["xinLinSuShi.jpg", "daDiWuShi.jpg", "caiJueZheP.jpg", "zhanXingJia.jpg"],"card":[],"skill":[],"audio":[]}
+    files:["xinLinSuShi.jpg", "daDiWuShi.jpg", "caiJueZheP.jpg", "zhanXingJia.jpg"],
 };
