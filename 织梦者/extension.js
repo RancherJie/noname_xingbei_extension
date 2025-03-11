acc0cae5eba55fa69a968170abd4d23ec2dd55b2
NongJie <245722665@qq.com>
2025-03-11 12:58:16 +0800

    测试

--- 织梦者/extension.js
+++ 织梦者/extension.js
@@ -1,4 +1,4 @@
-import { lib, game, ui, get, ai, _status } from "../../../noname.js";
+import { lib, game, ui, get, ai, _status } from "../../noname.js";
 export const type = "extension";
 export default function(){
 	return {name:"织梦者",arenaReady:function(){
