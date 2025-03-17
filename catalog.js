869a07b39d935aaffba94e8121ed4e75af73ae0e
NongJie <245722665@qq.com>
2025-03-17 16:16:15 +0800

    更新织梦者版本

--- catalog.js
+++ catalog.js
@@ -4,12 +4,12 @@ if (!localStorage.getItem('old-extension-tip')) {
 	localStorage.setItem('old-extension-tip', 'true');
 };
 extension["织梦者"]={
-	date:"2025/03/11",
+	date:"2025/03/17",
 	intro:"玩弄技能的角色。游戏本体版本最低v1.0.2",
 	author:"农之",
 	netdisk:"",
 	forum:"",
-	version:"1.1",
+	version:"1.2",
 	files:["info.json","LICENSE","mengJingSuXing.jpg","package.js","README.md","zhiMengZhe.jpg"],
 	size:"1.8MB"
 };
