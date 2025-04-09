0ab4838835aa70aac8d4ba34460be0decf4e593a
NongJie <245722665@qq.com>
2025-04-09 16:04:39 +0800

    删除扩展终末祷铸

--- catalog.js
+++ catalog.js
@@ -13,14 +13,3 @@ extension["织梦者"]={
 	files:["info.json","LICENSE","mengJingSuXing.jpg","package.js","README.md","zhiMengZhe.jpg"],
 	size:"1.8MB"
 };
-extension["终末祷铸"]={
-	intro:"三扩，本体最低版本1.0.20，反馈QQ群966951007",
-	date:'2025/04/08',
-	author:"农杰",
-	netdisk:"",
-	forum:"",
-	version:"4.1",
-	files:["info.json","README.md","LICENSE","character/hongYiZhuJiao.jpg",
-	"character/tieLvZhe.jpg","zhuanShu/wangQuanBaoZhu.png",'zhuanShu/shiShuCard.png','character/jiLuZhe.jpg','zhuanShu/luBiao.png','character/chuanJiaoShi.jpg','character/yiJiaoTu.jpg','zhuanShu/diLieBoTao.png','zhuanShu/moRiYuYan.png','zhuanShu/tianLeiJieHuo.png',],
-	size:"3.53MB",
-};
