3f943bca2a6995fd5110c916375a54a068a245ea
Mofish <13347880332@126.com>
2026-08-25 15:02:23 +0800

    同步十周年AI系列更新

--- 十周年-噩梦人机/package.js
+++ 十周年-噩梦人机/package.js
@@ -3,7 +3,7 @@ extension["十周年-噩梦人机"] = {
     author: "蒙牛 / Codex",
     netdisk: "",
     forum: "",
-    version: "0.1",
+    version: "1.1",
     files: [
         "info.json",
         "README.md",
